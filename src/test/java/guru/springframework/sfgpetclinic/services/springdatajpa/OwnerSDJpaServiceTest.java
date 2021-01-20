f93fe23f42562bace694f1df37991983d97db369
David Payne <djpdjp16@gmail.com>
2021-01-20 10:35:39 -0600

    safd

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -1,11 +1,8 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
-import guru.springframework.sfgpetclinic.model.Owner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @Disabled(value = "Disabled until we learn Mocking")
 class OwnerSDJpaServiceTest {
@@ -18,10 +15,9 @@ class OwnerSDJpaServiceTest {
 
     }
 
-    @Disabled
     @Test
     void findByLastName() {
-        Owner foundOwner = service.findByLastName("Buck");
+//        Owner foundOwner = service.findByLastName("Buck");
     }
 
     @Test
